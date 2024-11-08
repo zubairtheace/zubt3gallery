c2495c6ec5f4ae79fee46a3dee90c74ba48a8cbb
Zubair Tofy <tofy.zubair@gmail.com>
2024-11-08 17:37:12 +0400

    Updating Next Configs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    }
+};
 
 export default config;
