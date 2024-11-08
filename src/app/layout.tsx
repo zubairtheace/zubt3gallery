6b094118655cca874642d0f5f32b4eae8bd60378
Zubair Tofy <tofy.zubair@gmail.com>
2024-11-08 15:09:16 +0400

    Scaffold UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,24 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav() {
+  return (
+    <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b">
+      <div>Gallery</div>
+      <div>Sign In</div>
+    </nav>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${GeistSans.variable}`}>      
+      <body className="flex flex-col gap-4 ">
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }
