dc99876f5086092d7a01baa0799becb685cd1a2a
Zubair Tofy <tofy.zubair@gmail.com>
2024-11-12 14:26:55 +0400

    Authentication

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,13 @@
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
+export function TopNav() {
+    return (
+      <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b">
+        <div>Gallery</div>
+        <div>
+            <SignedOut><SignInButton/></SignedOut>
+            <SignedIn><UserButton/></SignedIn>
+        </div>
+      </nav>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import { db } from "~/server/db";
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
   });
 
-  return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
+  return(
+    <div className="flex flex-wrap gap-4">
         {[...images, ...images, ...images].map((image, index) => (
           <div key={`${image.id}-${index}`} className="flex flex-col w-48">
             <img src={image.url} alt="Gallery image" />
@@ -17,6 +18,21 @@ export default async function HomePage() {
           </div>
         ))}
       </div>
+  );
+}
+
+export default async function HomePage() {
+  
+
+  return (
+    <main className="">
+      <SignedOut>
+        <div className="w-full h-4 text-2xl text-center">
+          Please sign in to view the gallery
+        </div>
+      </SignedOut>
+      <SignedIn><Images /></SignedIn>
+      
     </main>
   );
 }
